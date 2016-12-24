54452a9645a1334f0ed3a14e2256a952454148df
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2016-12-24 15:24:07 +0200

    Added exercise 6.6 and README

---
+++ README.md
@@ -0,0 +1,3 @@
+Solutions to K&R C programming language Chapter 6 exercises.
+
+Exercise 6.6 is still unfinished.
--- exercise66.c
+++ exercise66.c
@@ -1,6 +1,12 @@
+// This is a solution to exercise 6.6 from K&R book.
+// The program implements a simple #define processor.
+//
+// Author: Raivydas Šimėnas
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define HASHSIZE 101
 #define BUFSIZE 1000
@@ -17,7 +23,39 @@ static struct nlist *hashtab[HASHSIZE];
 char buf[BUFSIZE];
 int bufp = 0;
 
+char getch() {
+  if (bufp != 0)
+    return buf[bufp--];
+  else
+    return getchar();
+}
+
+void ungetch(char c) {
+  if (bufp < BUFSIZE)
+    buf[bufp++] = c;
+  else
+    printf("The buffer is full.\n");
+}
+
+char *getword() {
+  char word[MAXLEN];
+  int currp = 0;
+  char currchar;
+  while ((currchar = getch()) != EOF && isalnum(currchar) && currp < MAXLEN)
+    word[currp++] = currchar;
+  while (!isalnum(currchar = getch()))
+    ;
+  if (isalnum(currchar))
+    ungetch(currchar);
+  word[currp] = '\0';
+  return word;
+}
+
 int main(int argc, char const *argv[]) {
-  printf("Hello, world\n");
+  char *word;
+  char c;
+  word = malloc(MAXLEN*(sizeof(char)));
+  word = getword();
+  printf("%s\n", word);
   return 0;
 }
